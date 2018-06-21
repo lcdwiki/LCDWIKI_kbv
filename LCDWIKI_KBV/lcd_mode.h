9841caa53fceb7086f38f288d563c03fe8fa453f
lcdwiki <39901169+lcdwiki@users.noreply.github.com>
2018-06-21 15:12:43 +0800

    Add files via upload

---
+++ LCDWIKI_KBV/lcd_mode.h
@@ -0,0 +1,8 @@
+#ifndef _lcd_mode_
+#define _lcd_mode_
+
+//if using 8bit mode,set the below macro definition to 1
+//if using 16bit mode,set the below macro definition to 0
+#define CONFIG_USE_8BIT_BUS 1
+
+#endif // _mcu_pin_magic_
