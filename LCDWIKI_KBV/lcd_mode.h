b290a6bce6aee52a8112beaef5ad138a9fb34292
lcdwiki <39901169+lcdwiki@users.noreply.github.com>
2018-06-21 15:21:09 +0800

    Delete lcd_mode.h

--- LCDWIKI_KBV/lcd_mode.h
+++
@@ -1,8 +0,0 @@
-#ifndef _lcd_mode_
-#define _lcd_mode_
-
-//if using 8bit mode,set the below macro definition to 1
-//if using 16bit mode,set the below macro definition to 0
-#define CONFIG_USE_8BIT_BUS 1
-
-#endif // _mcu_pin_magic_
